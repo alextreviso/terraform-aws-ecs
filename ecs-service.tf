e93e7e3fdd992b081e6981d467f2fffc2e014b0a
Alex Treviso <alextreviso12@gmail.com>
2022-07-12 23:45:47 -0300

    test subnets

--- ecs-service.tf
+++ ecs-service.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_service" "ecs-service" {
  
   network_configuration {
     security_groups  = [aws_security_group.sg-ecs-task.id]
-    subnets          = var.private_subnets_ids
+    subnets          = var.public_subnets_ids
     assign_public_ip = false
   }
 
