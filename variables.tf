4aeb1390fce62cbb393c7e82936194545fe6f80c
Alex Treviso <alextreviso12@gmail.com>
2022-07-13 12:27:42 -0300

    fix variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,14 @@
 variable "app_name" {}
 variable "env" {}
 variable "region" {}
-
 variable "vpc_id" {}
 variable "private_subnets_ids" {}
 variable "public_subnets_ids" {}
-
 variable "container_port" {}
 variable "task_memory" {}
 variable "task_cpu" {}
 variable "allowed_cidrs" {}
 variable "healthcheck_url" {}
-
 variable "private_access" {}
-
 variable "repository_url" {}
 
